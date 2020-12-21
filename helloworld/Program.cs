59aa7e4f4035d6b39299fe11faa80352c49253cd
Usama Yousaf <usamayousaf177@outlook.com>
2020-12-22 01:06:40 +0500

    My c# tutorial

---
+++ helloworld/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace helloworld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            byte number = 2;
+            int count = 10;
+            float totalPrice = 20.95f;
+            char character = 'A';
+            string firstName = "usama";
+            bool isWorking = false;
+            Console.WriteLine(number);
+            Console.WriteLine(count);
+            Console.WriteLine(totalPrice);
+            Console.WriteLine(character);
+            Console.WriteLine(firstName);
+            Console.WriteLine(isWorking);
+        }
+    }
+}
